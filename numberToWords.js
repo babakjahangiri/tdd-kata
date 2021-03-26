b723392468894c22b33852bbbf481c55307ed2e9
Babak <babakjahangiri@gmail.com>
2021-03-26 18:54:53 +0000

    write number conversion test

--- numberToWords.js
+++ numberToWords.js
@@ -1,3 +1,39 @@
 export function convertNumber(number) {
+  var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+  var dg = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  var tn = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  var tw = [
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
   return 'one';
 }
--- numberToWords.test.js
+++ numberToWords.test.js
@@ -1,11 +1,59 @@
-import {convertNumber} from './numberToWords'
+import { convertNumber } from './numberToWords';
 
-describe('the function should return the number words',()=>{
+describe('the function should return the number words', () => {
+  it('should return zero', () => {
+    const expected = 'zero';
+    const actual = convertNumber(0);
+    expect(actual).toBe(expected);
+  });
 
-    it('should return one',()=>{
-        const expected = 'one'
-        const actual = convertNumber(1)
-        expect(actual).toBe(expected)
-    })
+  it('should return one', () => {
+    const expected = 'one';
+    const actual = convertNumber(1);
+    expect(actual).toBe(expected);
+  });
 
-})
+  it('should return twenty-one', () => {
+    const expected = 'twenty-one';
+    const actual = convertNumber(21);
+    expect(actual).toBe(expected);
+  });
+
+  it('should return one hundred and twenty-three', () => {
+    const expected = 'one hundred and twenty-three';
+    const actual = convertNumber(123);
+    expect(actual).toBe(expected);
+  });
+
+  it('should return one thousand and five', () => {
+    const expected = 'one thousand and five';
+    const actual = convertNumber(1005);
+    expect(actual).toBe(expected);
+  });
+
+  it('should return one thousand and forty-two', () => {
+    const expected = 'one thousand and forty-two';
+    const actual = convertNumber(1042);
+    expect(actual).toBe(expected);
+  });
+
+  it('should return one thousand one hundred and five', () => {
+    const expected = 'one thousand one hundred and five';
+    const actual = convertNumber(1105);
+    expect(actual).toBe(expected);
+  });
+
+  it('should return fifty-six million nine hundred and forty-five thousand seven hundred and eighty-one', () => {
+    const expected =
+      'fifty-six million nine hundred and forty-five thousand seven hundred and eighty-one';
+    const actual = convertNumber(56945781);
+    expect(actual).toBe(expected);
+  });
+
+  it('should return nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine', () => {
+    const expected =
+      'nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine';
+    const actual = convertNumber(999999999);
+    expect(actual).toBe(expected);
+  });
+});
