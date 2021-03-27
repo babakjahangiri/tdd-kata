de6f0485027c310f281639d4c9a9f3a72d3462d8
Gennady Tabala <gtabala@gmail.com>
2021-03-27 00:19:49 +0000

    Added three new tests. Function is complit.

--- numberToWords.js
+++ numberToWords.js
@@ -1,26 +1,5 @@
 export function convertNumber(number) {
-  const getDigits = (number) => {
-    return dg[number];
-  };
-
-  const getTeens = (number) => {
-    return tn[number % 10];
-  };
-
-  const getTens = (number) => {
-    return `${tw[(number - (number % 10)) / 10]}-${dg[number % 10]}`;
-  };
-
-  const getBeforeHundred = (number) => {
-    if (number < 10) {
-      return getDigits(number);
-    } else if (number < 20) {
-      return getTeens(number);
-    } else if (number < 100) {
-      return getTens(number);
-    }
-  };
-
+  let and = 0;
   var th = ["", "thousand", "million", "billion", "trillion"];
   var dg = [
     "zero",
@@ -58,10 +37,57 @@ export function convertNumber(number) {
     "eighty",
     "ninety",
   ];
-  if (number < 100) {
-    return getBeforeHundred(number);
-  } else if (number < 1000) {
-    let tens = number % 100;
-    return `${dg[(number - tens) / 100]} hundred and ${getBeforeHundred(tens)}`;
-  }
+
+  const getGroupsOfDigit = (number, ind = 0, result = "") => {
+    if(ind > 4){
+      return "Error! Number is bigger than 999 999 999 999 999";
+    }
+    let hundred = number % 1000;
+    let thousand = (number - hundred) / 1000;
+    if (result) {
+      result = (ind + and) ? `${result}` : `and ${result}`;
+    }
+    result = `${(hundred||!result) ? getBeforeSandLion(hundred):""}${(th[ind]&&hundred) ? ` ${th[ind]} `:""}${result}`;
+    if (thousand > 0) {
+      return getGroupsOfDigit(thousand, ++ind, result);
+    }
+    return result;
+  };
+
+  const getDigits = (number) => {
+    return dg[number];
+  };
+
+  const getTeens = (number) => {
+    return tn[number % 10];
+  };
+
+  const getTens = (number) => {
+    return `${tw[(number - (number % 10)) / 10]}-${getDigits(number % 10)}`;
+  };
+
+  const getBeforeHundred = (number) => {
+    if (number < 10) {
+      return getDigits(number);
+    } else if (number < 20) {
+      return getTeens(number);
+    } else if (number < 100) {
+      return getTens(number);
+    }
+  };
+
+  const getBeforeSandLion = (number) => {
+    if (number < 100) {
+      and = -1
+      return getBeforeHundred(number);
+    } else if (number < 1000) {
+      ;
+      let tens = number % 100;
+      return `${getDigits((number - tens) / 100)} hundred${
+        tens ? ` and ${getBeforeHundred(tens)}` : ``
+      }`;
+    }
+  };
+
+  return getGroupsOfDigit(number);
 }
--- numberToWords.test.js
+++ numberToWords.test.js
@@ -30,36 +30,56 @@ describe('the function should return the number words', () => {
     const actual = convertNumber(123);
     expect(actual).toBe(expected);
   });
+  
+  it('should return five hundred', () => {
+    const expected = 'five hundred';
+    const actual = convertNumber(500);
+    expect(actual).toBe(expected);
+  });
 
-  xit('should return one thousand and five', () => {
+  it('should return one thousand and five', () => {
     const expected = 'one thousand and five';
     const actual = convertNumber(1005);
     expect(actual).toBe(expected);
   });
 
-  xit('should return one thousand and forty-two', () => {
+  it('should return one thousand and forty-two', () => {
     const expected = 'one thousand and forty-two';
     const actual = convertNumber(1042);
     expect(actual).toBe(expected);
   });
 
-  xit('should return one thousand one hundred and five', () => {
+  it('should return one thousand one hundred and five', () => {
     const expected = 'one thousand one hundred and five';
     const actual = convertNumber(1105);
     expect(actual).toBe(expected);
   });
 
-  xit('should return fifty-six million nine hundred and forty-five thousand seven hundred and eighty-one', () => {
+  it('should return fifty-six million nine hundred and forty-five thousand seven hundred and eighty-one', () => {
     const expected =
       'fifty-six million nine hundred and forty-five thousand seven hundred and eighty-one';
     const actual = convertNumber(56945781);
     expect(actual).toBe(expected);
   });
+  
+  it('should return fifty-six million seven hundred and eighty-one', () => {
+    const expected =
+      'fifty-six million seven hundred and eighty-one';
+    const actual = convertNumber(56000781);
+    expect(actual).toBe(expected);
+  });
 
-  xit('should return nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine', () => {
+  it('should return nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine', () => {
     const expected =
       'nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine';
     const actual = convertNumber(999999999);
+    expect(actual).toBe(expected);
+  });
+ 
+  it('should return "Error! Number is bigger than 999 999 999 999 999"', () => {
+    const expected =
+      'Error! Number is bigger than 999 999 999 999 999';
+    const actual = convertNumber(1000000000000001);
     expect(actual).toBe(expected);
   });
 });
